790dc03245c01e4562c71e2a462a825a535bde13
cyotee doge <cyotee@gmail.com>
2020-11-18 04:37:47 -0800

    Moved code other repos

--- contracts/dependencies/interfaces/uniswapV2/IUniswapV2Callee.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.7.4;
-
-import "hardhat/console.sol";
-
-interface IUniswapV2Callee {
-    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
-}
