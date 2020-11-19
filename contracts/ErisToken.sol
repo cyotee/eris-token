9248c45ce0a66536bbdcbf5763b5a45ad9a1ef30
cyotee doge <cyotee@gmail.com>
2020-11-19 03:07:39 -0800

    Commented out WIP code

--- contracts/ErisToken.sol
+++ contracts/ErisToken.sol
@@ -3,17 +3,13 @@ pragma solidity 0.7.4;
 
 import "hardhat/console.sol";
 
-import "./abstract/Divine.sol";
+// import "./abstract/Divine.sol";
 // import "./libraries/utils/math/SafeMath.sol";
 // import "./libraries/datatypes/primitives/Address.sol";
 // import "../libraries/datatypes/collections/EnumerableSet.sol";
 
 contract ErisToken is Divine {
-
-    // using SafeMath for uint256;
-    // using Address for address;
-    // using EnumerableSet for EnumerableSet.AddressSet;
-
+    
     constructor () Divine() {
         console.log("ERIS::constructor: Instantiating ERIS");
         // _mint(_msgSender(), 50000 * 1**decimals()  );
--- test/contracts/TestToken1.sol
+++ test/contracts/TestToken1.sol
@@ -1,20 +1,20 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity 0.7.4;
+// // SPDX-License-Identifier: AGPL-3.0-or-later
+// pragma solidity 0.7.4;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
-import "../../contracts/abstract/ERC20.sol";
-import "../../contracts/libraries/utils/math/SafeMath.sol";
-import "../../contracts/libraries/datatypes/primitives/Address.sol";
+// import "../../contracts/abstract/ERC20.sol";
+// import "../../contracts/libraries/utils/math/SafeMath.sol";
+// import "../../contracts/libraries/datatypes/primitives/Address.sol";
 
-contract TestToken1 is ERC20 {
+// contract TestToken1 is ERC20 {
 
-    using SafeMath for uint256;
-    using Address for address;
+//     using SafeMath for uint256;
+//     using Address for address;
 
-    constructor () ERC20( "TestToken1", "TT1" ) {
-        console.log("TestToken1::constructor: Instantiating TestToken1");
-        _mint(_msgSender(), 50000 * 1**decimals()  );
-        console.log("TestToken1::constructor: Instantiated TestToken1");
-    }
-}
+//     constructor () ERC20( "TestToken1", "TT1" ) {
+//         console.log("TestToken1::constructor: Instantiating TestToken1");
+//         _mint(_msgSender(), 50000 * 1**decimals()  );
+//         console.log("TestToken1::constructor: Instantiated TestToken1");
+//     }
+// }
